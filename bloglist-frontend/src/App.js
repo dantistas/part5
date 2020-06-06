d0d0553835d757b46a03820ce96426f40be4a7fb
dantistas <rytisvenslovas@gmail.com>
2020-06-06 17:52:36 +0200

    5.2

--- bloglist-frontend/src/App.js
+++ bloglist-frontend/src/App.js
@@ -7,7 +7,7 @@ const App = () => {
   const [blogs, setBlogs] = useState([])
   const [username, setUsername] = useState('')
   const [password, setPassword] = useState('')
-  const [errorMessage, setErrorMessage] = useState('')
+  const [errorMessage, setErrorMessage] = useState(null)
   const [user, setUser] = useState(null)
 
   useEffect(() => {
@@ -15,6 +15,13 @@ const App = () => {
       setBlogs( blogs )
     )  
   }, [])
+  useEffect(()=>{
+    const loggedUserJson = window.localStorage.getItem('loggedUser') 
+    if(loggedUserJson){
+      const user = JSON.parse(loggedUserJson)
+      setUser(user)
+    }
+  },[])
   const handleLogin = async (e) => {
     e.preventDefault()
     console.log(`logging with username: ${username} password: ${password}`)
@@ -22,6 +29,7 @@ const App = () => {
       const user = await loginService.login({
         username,password,
       })
+      window.localStorage.setItem('loggedUser', JSON.stringify(user))
       setUser(user)
       setUsername('')
       setPassword('')
@@ -60,11 +68,16 @@ const App = () => {
       </div>
     )
   }
-
+const logOut = ()=>{
+  window.localStorage.clear()
+  window.location.reload()
+  
+}
   return (
     <div>
       <h2>blogs</h2>
       <p>logged in as {user.name} </p>
+      <button onClick={logOut}>log out</button>
       {blogs.map(blog =>
         <Blog key={blog.id} blog={blog} />
       )}
